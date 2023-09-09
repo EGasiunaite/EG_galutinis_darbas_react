0111bdf3a3c94a6c685daa366bd6cdbce604bb5f
EvelinaGasiuniate <evelina.gasiunaite@gmail.com>
2023-09-09 22:50:00 +0300

    Hero  and  header styling

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -8,7 +8,7 @@ function OneLink(props) {
     return (
       <NavLink
         onClick={props.onClick}
-        className={'text-lg px-3 py-2 hover:bg-slate-200'}
+        className={'text-lg px-3 py-2 hover:text-red-500'}
         to={props.to}
       >
         {props.title}
@@ -22,7 +22,7 @@ function OneLink(props) {
     signOut(auth)
       .then(() => {
         // Sign-out successful.
-        toast.success('Atsijungete');
+        toast.success('You have logged out');
       })
       .catch((error) => {
         // An error happened.
@@ -34,11 +34,12 @@ export default function Header() {
     const ctx = useAuth();
     console.log('ctx ===', ctx);
     return (
-      <header className='container flex justify-between items-center bg-slate-800 text-white py-4 px-6'>
+      <header className='bg-slate-800 text-white py-4'>
+      <div className="container mx-auto px-6 flex justify-between items-center">
         <Link to={'/'}>
         <img src={logo} alt="Super SHOP Logo" className="h-12" />
         </Link>
-        <nav>
+        <nav className="flex space-x-3">
           {ctx.isUserLoggedIn && (
             <>
               <OneLink to={'/shops'} title={'Shops'} />
@@ -46,10 +47,10 @@ export default function Header() {
             </>
           )}
           {ctx.isUserLoggedIn === false && (
-            <OneLink to={'/login'} title={'Login'} />
-          )}
-          {ctx.isUserLoggedIn === false && (
-            <OneLink to={'/register'} title={'Register'} />
+            <>
+              <OneLink to={'/login'} title={'Login'} />
+              <OneLink to={'/register'} title={'Register'} />
+            </>
           )}
           {ctx.isUserLoggedIn && (
             <OneLink onClick={logoutFire} to={'/login'} title={'Logout'} />
@@ -58,6 +59,7 @@ export default function Header() {
             <p className='inline-block text-lg px-3 py-2'>{ctx.email}</p>
           )} */}
         </nav>
+        </div>
       </header>
     );
   }
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import mallImage from '../components/layout/mall.jpg';
 
 const LandingPage = () => {
+  const heroSectionStyle = {
+    backgroundImage: `url(${mallImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  };
+
+
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-md text-center">
-        <h1 className="text-3xl font-semibold mb-4">Welcome to Ozas Website</h1>
+    <div className="min-h-screen flex items-center justify-center" style={heroSectionStyle}>
+      <div className="p-8 text-center">
+        <h1 className="text-3xl text-white font-semibold mb-4">Welcome to Ozas Website</h1>
         <div className="flex justify-center space-x-4">
-        <Link to="/login" className="bg-gray-800 hover:bg-gray-900 text-white px-6 py-3 rounded-lg">
+          <Link
+            to="/login"
+            className="px-40 py-4 text-xl rounded-lg transition bg-white hover:bg-red-600 hover:text-white"
+          >
             Login
           </Link>
-          <Link to="/register" className="bg-gray-800 hover:bg-gray-900 text-white px-6 py-3 rounded-lg">
+          <Link
+            to="/register"
+            className="px-40 py-4 text-xl rounded-lg transition bg-white hover:bg-red-600 hover:text-white"
+          >
             Register
           </Link>
         </div>
