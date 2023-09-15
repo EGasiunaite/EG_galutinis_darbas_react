2276c4c291a75d393ee5aa8b5df1e79ac4f2f681
EvelinaGasiuniate <evelina.gasiunaite@gmail.com>
2023-09-15 13:38:33 +0300

    Footer shop list changes

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,21 +1,46 @@
 import React from 'react';
-import { FaFacebook } from 'react-icons/fa';
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
     <footer className="bg-slate-800 text-white py-6">
-      <div className="container flex justify-between items-center">
+      <div className="container grid grid-cols-2 md:grid-cols-4 gap-4">
         <div className="text-sm">
-          <p >Contact Us</p>
+          <p>Contact Us</p>
           <p>Email: contact@evelina.com</p>
           <p>Phone: +370-370-370</p>
         </div>
-        <div>
+        <div className="text-sm">
+          <p>Legal</p>
+          <p>Privacy Policy</p>
+          <p>Terms of Use</p>
+          <p>Cookie Policy</p>
+          <p>Cookie Settings</p>
+        </div>
+        <div className="text-sm">
+          <p>Explore</p>
+          <p>Shopping</p>
+          <p>Services</p>
+
+        </div>
+        <div className="flex space-x-4 col-span-2 md:col-span-1">
           <Link to="https://www.facebook.com/ozas.lt/" target="_blank" rel="noopener noreferrer">
             <FaFacebook className="text-3xl hover:text-blue-500" />
           </Link>
+          <Link to="https://www.twitter.com/ozas.lt/" target="_blank" rel="noopener noreferrer">
+            <FaTwitter className="text-3xl hover:text-blue-500" />
+          </Link>
+          <Link to="https://www.instagram.com/ozas.lt/" target="_blank" rel="noopener noreferrer">
+            <FaInstagram className="text-3xl hover:text-blue-500" />
+          </Link>
+          <Link to="https://www.linkedin.com/ozas.lt/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin className="text-3xl hover:text-blue-500" />
+          </Link>
         </div>
+      </div>
+      <div className="text-center mt-4">
+        <p>&copy; 2023 Evelina Ltd. All Rights Reserved.</p>
       </div>
     </footer>
   );
--- src/pages/AddShopPage.jsx
+++ src/pages/AddShopPage.jsx
@@ -20,25 +20,34 @@ function AddShop() {
     shopImg: '',
   };
 
-  // Yup'as
-  const validationSchema = Yup.object({
-    shopName: Yup.string().min(4).max(100).required('Shop name is required'),
-    town: Yup.string().min(4).max(100).required('Town is required'),
-    startYear: Yup.number()
+ // Yup'as
+const validationSchema = Yup.object({
+  shopName: Yup.string()
+    .min(4, 'Shop name must be at least 4 characters')
+    .max(100, 'Shop name must be at most 100 characters')
+    .required('Shop name is required'),
+  town: Yup.string()
+    .min(4, 'Town must be at least 4 characters')
+    .max(100, 'Town must be at most 100 characters')
+    .required('Town is required'),
+  startYear: Yup.number()
     .required('Year is required')
     .min(1970, 'Year must be at least 1970')
     .max(2025, 'Year must be at most 2025'),
-    description: Yup.string().required('Description is required'),
-    logoImg: Yup.string()
-      .required('Main Image URL is required')
-      .url('Invalid URL'),
-    phoneNumber: Yup.number(),
-    website: Yup.string()
+  description: Yup.string().required('Description is required'),
+  logoImg: Yup.string()
+    .required('Main Image URL is required')
     .url('Invalid URL'),
-    workingHours: Yup.string().min(4).max(100),
-    shopImg: Yup.string()
-      .url('Invalid URL'),
-  });
+  phoneNumber: Yup.number(),
+  website: Yup.string()
+    .url('Invalid URL'),
+  workingHours: Yup.string()
+    .min(4, 'Working hours must be at least 4 characters')
+    .max(100, 'Working hours must be at most 100 characters'),
+  shopImg: Yup.string()
+    .url('Invalid URL'),
+});
+
 
   // Formik configuration
   const formik = useFormik({
@@ -68,227 +77,228 @@ function AddShop() {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-md text-center mt-20 mb-20">
+<div className="min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-md text-center mt-20 mb-20 w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3">
         <h2 className="text-3xl font-semibold mb-4">Add a shop</h2>
-        <form onSubmit={formik.handleSubmit}className="max-w-xs mx-auto">
-
-           {/* shopName */}
-           <div className="mb-4">
-            <label htmlFor="shopName">Shop Name</label>
-            <input
-              type="text"
-              id="shopName"
-              name="shopName"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.shopName}
-              className={`mt-1 p-2 w-full border rounded-md ${
-                formik.touched.shopName && formik.errors.shopName
-                  ? 'border-red-500 focus:border-red-500'
-                  : 'border-gray-300 focus:border-blue-500'
-              }`}
-            />
-            {formik.touched.shopName && formik.errors.shopName && (
-              <div className="text-red-500 text-sm mt-1">
-                {formik.errors.shopName}
+        <div className="flex flex-wrap">
+          {/* Left Column */}
+          <div className="w-full sm:w-1/2 p-2">
+            <form onSubmit={formik.handleSubmit}>
+              {/* shopName */}
+              <div className="mb-4">
+                <label htmlFor="shopName">Shop Name</label>
+                <input
+                  type="text"
+                  id="shopName"
+                  name="shopName"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.shopName}
+                  className={`mt-1 p-2 w-full border rounded-md ${
+                    formik.touched.shopName && formik.errors.shopName
+                      ? 'border-red-500 focus:border-red-500'
+                      : 'border-gray-300 focus:border-blue-500'
+                  }`}
+                />
+                {formik.touched.shopName && formik.errors.shopName && (
+                  <div className="text-red-500 text-sm mt-1">
+                    {formik.errors.shopName}
+                  </div>
+                )}
               </div>
-            )}
-          </div>
-           {/* town */}
-           <div className="mb-4">
-            <label htmlFor="town">Town</label>
-            <input
-              type="text"
-              id="town"
-              name="town"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.town}
-              className={`mt-1 p-2 w-full border rounded-md ${
-                formik.touched.town && formik.errors.town
-                  ? 'border-red-500 focus:border-red-500'
-                  : 'border-gray-300 focus:border-blue-500'
-              }`}
-            />
-            {formik.touched.town && formik.errors.town && (
-              <div className="text-red-500 text-sm mt-1">
-                {formik.errors.town}
+              {/* town */}
+              <div className="mb-4">
+                <label htmlFor="town">Town</label>
+                <input
+                  type="text"
+                  id="town"
+                  name="town"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.town}
+                  className={`mt-1 p-2 w-full border rounded-md ${
+                    formik.touched.town && formik.errors.town
+                      ? 'border-red-500 focus:border-red-500'
+                      : 'border-gray-300 focus:border-blue-500'
+                  }`}
+                />
+                {formik.touched.town && formik.errors.town && (
+                  <div className="text-red-500 text-sm mt-1">
+                    {formik.errors.town}
+                  </div>
+                )}
               </div>
-            )}
-          </div>
-
-          {/* Description */}
-          <div className="mb-4">
-            <label htmlFor="description">Description</label>
-            <textarea
-              id="description"
-              name="description"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.description}
-              className={`mt-1 p-2 w-full border rounded-md ${
-                formik.touched.description && formik.errors.description
-                  ? 'border-red-500 focus:border-red-500'
-                  : 'border-gray-300 focus:border-blue-500'
-              }`}
-            />
-            {formik.touched.description && formik.errors.description && (
-              <div className="text-red-500 text-sm mt-1">
-                {formik.errors.description}
+              {/* Description */}
+              <div className="mb-4">
+                <label htmlFor="description">Description</label>
+                <textarea
+                  id="description"
+                  name="description"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.description}
+                  className={`mt-1 p-2 w-full border rounded-md ${
+                    formik.touched.description && formik.errors.description
+                      ? 'border-red-500 focus:border-red-500'
+                      : 'border-gray-300 focus:border-blue-500'
+                  }`}
+                />
+                {formik.touched.description && formik.errors.description && (
+                  <div className="text-red-500 text-sm mt-1">
+                    {formik.errors.description}
+                  </div>
+                )}
               </div>
-            )}
-          </div>
-
-          {/* Start Year */}
-          <div className="mb-4">
-            <label htmlFor="startYear">Start Year</label>
-            <input
-              type="number"
-              id="startYear"
-              name="startYear"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.startYear}
-              className={`mt-1 p-2 w-full border rounded-md ${
-                formik.touched.startYear && formik.errors.startYear
-                  ? 'border-red-500 focus:border-red-500'
-                  : 'border-gray-300 focus:border-blue-500'
-              }`}
-            />
-            {formik.touched.startYear && formik.errors.startYear && (
-              <div className="text-red-500 text-sm mt-1">
-                {formik.errors.startYear}
+              {/* Start Year */}
+              <div className="mb-4">
+                <label htmlFor="startYear">Start Year</label>
+                <input
+                  type="number"
+                  id="startYear"
+                  name="startYear"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.startYear}
+                  className={`mt-1 p-2 w-full border rounded-md ${
+                    formik.touched.startYear && formik.errors.startYear
+                      ? 'border-red-500 focus:border-red-500'
+                      : 'border-gray-300 focus:border-blue-500'
+                  }`}
+                />
+                {formik.touched.startYear && formik.errors.startYear && (
+                  <div className="text-red-500 text-sm mt-1">
+                    {formik.errors.startYear}
+                  </div>
+                )}
               </div>
-            )}
-          </div>
-
-          {/* Logo Image URL */}
-          <div className="mb-4">
-            <label htmlFor="logoImg">Logo URL</label>
-            <input
-              type="text"
-              id="logoImg"
-              name="logoImg"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.logoImg}
-              className={`mt-1 p-2 w-full border rounded-md ${
-                formik.touched.logoImg && formik.errors.logoImg
-                  ? 'border-red-500 focus:border-red-500'
-                  : 'border-gray-300 focus:border-blue-500'
-              }`}
-            />
-            {formik.touched.logoImg && formik.errors.logoImg && (
-              <div className="text-red-500 text-sm mt-1">
-                {formik.errors.logoImg}
+              {/* Logo Image URL */}
+              <div className="mb-4">
+                <label htmlFor="logoImg">Logo URL</label>
+                <input
+                  type="text"
+                  id="logoImg"
+                  name="logoImg"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.logoImg}
+                  className={`mt-1 p-2 w-full border rounded-md ${
+                    formik.touched.logoImg && formik.errors.logoImg
+                      ? 'border-red-500 focus:border-red-500'
+                      : 'border-gray-300 focus:border-blue-500'
+                  }`}
+                />
+                {formik.touched.logoImg && formik.errors.logoImg && (
+                  <div className="text-red-500 text-sm mt-1">
+                    {formik.errors.logoImg}
+                  </div>
+                )}
               </div>
-            )}
+            </form>
           </div>
-
-          <h3 className="text-2xl font-semibold mb-4">Additional information</h3>
-
-          {/* phoneNumber */}
-<div className="mb-4">
-  <label htmlFor="phoneNumber">Phone Number</label>
-  <input
-    type="text"
-    id="phoneNumber"
-    name="phoneNumber"
-    onChange={formik.handleChange}
-    onBlur={formik.handleBlur}
-    value={formik.values.phoneNumber}
-    className={`mt-1 p-2 w-full border rounded-md ${
-      formik.touched.phoneNumber && formik.errors.phoneNumber
-        ? 'border-red-500 focus:border-red-500'
-        : 'border-gray-300 focus:border-blue-500'
-    }`}
-  />
-  {formik.touched.phoneNumber && formik.errors.phoneNumber && (
-    <div className="text-red-500 text-sm mt-1">
-      {formik.errors.phoneNumber}
-    </div>
-  )}
-</div>
-
-{/* website */}
-<div className="mb-4">
-  <label htmlFor="website">Website</label>
-  <input
-    type="text"
-    id="website"
-    name="website"
-    onChange={formik.handleChange}
-    onBlur={formik.handleBlur}
-    value={formik.values.website}
-    className={`mt-1 p-2 w-full border rounded-md ${
-      formik.touched.website && formik.errors.website
-        ? 'border-red-500 focus:border-red-500'
-        : 'border-gray-300 focus:border-blue-500'
-    }`}
-  />
-  {formik.touched.website && formik.errors.website && (
-    <div className="text-red-500 text-sm mt-1">
-      {formik.errors.website}
-    </div>
-  )}
-</div>
-
-{/* workingHours */}
-<div className="mb-4">
-  <label htmlFor="workingHours">Working Hours</label>
-  <input
-    type="text"
-    id="workingHours"
-    name="workingHours"
-    onChange={formik.handleChange}
-    onBlur={formik.handleBlur}
-    value={formik.values.workingHours}
-    className={`mt-1 p-2 w-full border rounded-md ${
-      formik.touched.workingHours && formik.errors.workingHours
-        ? 'border-red-500 focus:border-red-500'
-        : 'border-gray-300 focus:border-blue-500'
-    }`}
-  />
-  {formik.touched.workingHours && formik.errors.workingHours && (
-    <div className="text-red-500 text-sm mt-1">
-      {formik.errors.workingHours}
-    </div>
-  )}
-</div>
-
-{/* shopImg */}
-<div className="mb-4">
-  <label htmlFor="shopImg">Shop Image URL</label>
-  <input
-    type="text"
-    id="shopImg"
-    name="shopImg"
-    onChange={formik.handleChange}
-    onBlur={formik.handleBlur}
-    value={formik.values.shopImg}
-    className={`mt-1 p-2 w-full border rounded-md ${
-      formik.touched.shopImg && formik.errors.shopImg
-        ? 'border-red-500 focus:border-red-500'
-        : 'border-gray-300 focus:border-blue-500'
-    }`}
-  />
-  {formik.touched.shopImg && formik.errors.shopImg && (
-    <div className="text-red-500 text-sm mt-1">
-      {formik.errors.shopImg}
-    </div>
-  )}
-</div>
-
-          <div className="mt-4">
-            <button
-              type="submit"
-              className="bg-slate-800 text-white px-4 py-2 rounded hover:bg-slate-500"
-            >
-              Submit
-            </button>
+          
+          {/* Right Column */}
+          <div className="w-full sm:w-1/2 p-2">
+            <form onSubmit={formik.handleSubmit}>
+              {/* phoneNumber */}
+              <div className="mb-4">
+                <label htmlFor="phoneNumber">Phone Number</label>
+                <input
+                  type="text"
+                  id="phoneNumber"
+                  name="phoneNumber"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.phoneNumber}
+                  className={`mt-1 p-2 w-full border rounded-md ${
+                    formik.touched.phoneNumber && formik.errors.phoneNumber
+                      ? 'border-red-500 focus:border-red-500'
+                      : 'border-gray-300 focus:border-blue-500'
+                  }`}
+                />
+                {formik.touched.phoneNumber && formik.errors.phoneNumber && (
+                  <div className="text-red-500 text-sm mt-1">
+                    {formik.errors.phoneNumber}
+                  </div>
+                )}
+              </div>
+              {/* website */}
+              <div className="mb-4">
+                <label htmlFor="website">Website</label>
+                <input
+                  type="text"
+                  id="website"
+                  name="website"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.website}
+                  className={`mt-1 p-2 w-full border rounded-md ${
+                    formik.touched.website && formik.errors.website
+                      ? 'border-red-500 focus:border-red-500'
+                      : 'border-gray-300 focus:border-blue-500'
+                  }`}
+                />
+                {formik.touched.website && formik.errors.website && (
+                  <div className="text-red-500 text-sm mt-1">
+                    {formik.errors.website}
+                  </div>
+                )}
+              </div>
+              {/* workingHours */}
+              <div className="mb-4">
+                <label htmlFor="workingHours">Working Hours</label>
+                <input
+                  type="text"
+                  id="workingHours"
+                  name="workingHours"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.workingHours}
+                  className={`mt-1 p-2 w-full border rounded-md ${
+                    formik.touched.workingHours && formik.errors.workingHours
+                      ? 'border-red-500 focus:border-red-500'
+                      : 'border-gray-300 focus:border-blue-500'
+                  }`}
+                />
+                {formik.touched.workingHours && formik.errors.workingHours && (
+                  <div className="text-red-500 text-sm mt-1">
+                    {formik.errors.workingHours}
+                  </div>
+                )}
+              </div>
+              {/* shopImg */}
+              <div className="mb-4">
+                <label htmlFor="shopImg">Shop Image URL</label>
+                <input
+                  type="text"
+                  id="shopImg"
+                  name="shopImg"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.shopImg}
+                  className={`mt-1 p-2 w-full border rounded-md ${
+                    formik.touched.shopImg && formik.errors.shopImg
+                      ? 'border-red-500 focus:border-red-500'
+                      : 'border-gray-300 focus:border-blue-500'
+                  }`}
+                />
+                {formik.touched.shopImg && formik.errors.shopImg && (
+                  <div className="text-red-500 text-sm mt-1">
+                    {formik.errors.shopImg}
+                  </div>
+                )}
+              </div>
+            </form>
           </div>
-        </form>
+        </div>
+
+        <div className="mt-4">
+        <button
+            type="submit"
+            className="bg-slate-800 text-white px-4 py-2 rounded-md hover:bg-slate-500 w-full"
+          >
+            Submit
+          </button>
+        </div>
       </div>
     </div>
   );
